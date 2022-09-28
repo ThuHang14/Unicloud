4cbb4eaf98262a7441cf8d6e0cd7155502fd33c3
ThuHang14 <14thuhang@gmail.com>
2022-09-28 10:00:14 +0700

    day 2

---
+++ DemoHeapStack/src/main/java/TestTryCatch.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+
+public class TestTryCatch {
+    public static void main(String[] args) {
+        int a = 3;
+//        THROW
+        if (a <= 0) throw new RuntimeException("nho hon khong ");
+        else System.out.println("a lon hon khong ");
+
+//        TROWS
+        M  m = new M();
+        try {
+            m.method();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+//        TRY_CATCH
+        try {
+            System.out.println(3 / a);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            System.out.println("end");
+        }
+
+        System.out.println(222);
+    }
+
+   static class M {
+        void method() throws IOException {
+            throw new IOException("Loi thiet bi");
+        }
+    }
+}
