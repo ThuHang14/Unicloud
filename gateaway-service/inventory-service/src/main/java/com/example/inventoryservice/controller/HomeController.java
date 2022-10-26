89c38dd0ff4dc7aec0f5edb4861293298a29a328
hangntt04 <hangntt04@unicloud.com.vn>
2022-10-26 13:55:37 +0700

    config all

--- gateaway-service/inventory-service/src/main/java/com/example/inventoryservice/controller/HomeController.java
+++ gateaway-service/inventory-service/src/main/java/com/example/inventoryservice/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.example.inventoryservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/api/test")
+@RefreshScope
 public class HomeController {
+
+    @Value("${test.name}")
+    private String name;
+
 @GetMapping
     public String home(){
-    return "home";
+    return name;
 }
 }
